465381c614035eb1cadb0413df76fc19afa00da6
nanoziros <nero.lucho92@gmail.com>
2017-01-22 09:53:26 -0400

    lhhhiol

--- Assets/Scripts/UI/PerPlayerUIPresenter.cs
+++ Assets/Scripts/UI/PerPlayerUIPresenter.cs
@@ -1,22 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PerPlayerUIPresenter : MonoBehaviour
 {
     public Image Health;
+    public float AnimationLenght = 0.35f;
 
+    private int oldHealth = -1;
     private PlayerController _playerController;
 
     public void Initialize(PlayerController playerController)
     {
         _playerController = playerController;
+
+        oldHealth = _playerController.HitPoints;
     }
 
     private void Update()
     {
         // Update health
-        this.Health.fillAmount = (float)this._playerController.HitPoints/this._playerController.MaxHitPoints;
+        if (oldHealth > _playerController.HitPoints)
+        {
+            oldHealth = _playerController.HitPoints;
+
+            this.Health.DOKill();
+
+            this.Health.DOFillAmount((float) this._playerController.HitPoints/this._playerController.MaxHitPoints,
+                AnimationLenght);
+        }
     }
 }
